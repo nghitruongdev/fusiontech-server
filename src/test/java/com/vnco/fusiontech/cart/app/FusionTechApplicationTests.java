5d2383a5593914b9910474623ae37aab71fd4cb4
nghitruong <89492293+vntruongdev@users.noreply.github.com>
2023-05-17 16:41:24 +0700

    Added cart module

--- src/test/java/com/vnco/fusiontech/cart/app/FusionTechApplicationTests.java
+++ src/test/java/com/vnco/fusiontech/cart/app/FusionTechApplicationTests.java
@@ -1,10 +1,10 @@
-package com.vnco.fusiontech.app;
+package com.vnco.fusiontech.cart.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FusiontechappApplicationTests {
+class FusionTechApplicationTests {
     
     @Test
     void contextLoads() {

6cba20811dacf112e7a05a05ddae61d5f315c6dc
nghitruong <89492293+vntruongdev@users.noreply.github.com>
2023-05-17 16:30:22 +0700

    Initialized spring boot project

---
+++ src/main/java/com/vnco/fusiontech/app/FusionTechApplication.java
@@ -0,0 +1,13 @@
+package com.vnco.fusiontech.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusionTechApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(FusionTechApplication.class, args);
+    }
+    
+}

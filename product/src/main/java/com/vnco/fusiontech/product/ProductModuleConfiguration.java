6edd426be8e8189a984cdf04c152ce55f2492909
nghitruong <89492293+vntruongdev@users.noreply.github.com>
2023-05-18 14:16:08 +0700

    Added common module and import modules into app

---
+++ product/src/main/java/com/vnco/fusiontech/product/ProductModuleConfiguration.java
@@ -0,0 +1,14 @@
+package com.vnco.fusiontech.product;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+//@EntityScan (basePackages = "com.vnco.order.entity")
+//@EnableJpaRepositories (basePackages = "com.vnco.order.repository")
+public class ProductModuleConfiguration {
+
+}
--- src/main/java/com/vnco/fusiontech/cart/app/FusionTechApplication.java
+++ src/main/java/com/vnco/fusiontech/cart/app/FusionTechApplication.java
@@ -1,9 +1,18 @@
 package com.vnco.fusiontech.cart.app;
 
+import com.vnco.fusiontech.cart.CartModuleConfiguration;
+import com.vnco.fusiontech.product.ProductModuleConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import (
+        {
+                CartModuleConfiguration.class,
+                ProductModuleConfiguration.class
+        }
+)
 public class FusionTechApplication {
     
     public static void main(String[] args) {

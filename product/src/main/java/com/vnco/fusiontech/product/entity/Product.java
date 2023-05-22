eee5480141ac2a7551caac17c9868d42c99934cd
Le Thanh Tu <letu060802@gmail.com>
2023-05-22 13:44:57 +0700

    entity add

--- product/src/main/java/com/vnco/fusiontech/product/entity/Product.java
+++ product/src/main/java/com/vnco/fusiontech/product/entity/Product.java
@@ -1,4 +1,36 @@
 package com.vnco.fusiontech.product.entity;
 
-public class Product {
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+@SuppressWarnings("serial")
+@Data
+@Entity
+@Table(name = "product")
+public class Product implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    int id;
+
+    @Column(name = "name", length = 255)
+    String name;
+
+    @Column(name = "description", length = 255)
+    String description;
+
+    @Column(name = "image", length = 255)
+    String image;
+
+    @Column(name = "quantity")
+    int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    Brand brand;
 }

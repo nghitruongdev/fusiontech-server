655fc2d8138d2ae771c7c332e8a707beb4b6c0f1
Le Thanh Tu <letu060802@gmail.com>
2023-05-25 14:27:46 +0700

    update service

--- product/src/main/java/com/vnco/fusiontech/product/repository/AttributeRepository.java
+++ product/src/main/java/com/vnco/fusiontech/product/repository/AttributeRepository.java
@@ -3,6 +3,8 @@ package com.vnco.fusiontech.product.repository;
 import com.vnco.fusiontech.product.entity.Attribute;
 import com.vnco.fusiontech.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface AttributeRepository extends JpaRepository<Attribute, Integer> {
+public interface AttributeRepository extends JpaRepository<Attribute, Long> {
+
 }

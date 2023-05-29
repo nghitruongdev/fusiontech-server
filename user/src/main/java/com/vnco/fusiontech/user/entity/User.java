445097fc619b25fa32b04d68b2471d40e4ae149a
xuaanhaor <eji0nkiz@gmail.com>
2023-05-29 10:47:06 +0700

    user entity getter & setter added

--- user/src/main/java/com/vnco/fusiontech/user/entity/User.java
+++ user/src/main/java/com/vnco/fusiontech/user/entity/User.java
@@ -1,9 +1,14 @@
 package com.vnco.fusiontech.user.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Objects;
 
+@Getter
+@Setter
 @Entity
 public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,46 +27,6 @@ public class User {
     @Basic
     @Column(name = "phone")
     private String phone;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPasswordHash() {
-        return passwordHash;
-    }
-
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 
     @Override
     public boolean equals(Object o) {

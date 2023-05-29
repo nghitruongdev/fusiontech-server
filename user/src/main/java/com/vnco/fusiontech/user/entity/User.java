44ebf25800cd471331976efeb90db6b619891dfc
xuaanhaor <eji0nkiz@gmail.com>
2023-05-29 10:45:30 +0700

    user entity added

---
+++ user/src/main/java/com/vnco/fusiontech/user/entity/User.java
@@ -0,0 +1,78 @@
+package com.vnco.fusiontech.user.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class User {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Basic
+    @Column(name = "username")
+    private String username;
+    @Basic
+    @Column(name = "password_hash")
+    private String passwordHash;
+    @Basic
+    @Column(name = "email")
+    private String email;
+    @Basic
+    @Column(name = "phone")
+    private String phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(username, user.username) && Objects.equals(passwordHash, user.passwordHash) && Objects.equals(email, user.email) && Objects.equals(phone, user.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, passwordHash, email, phone);
+    }
+}

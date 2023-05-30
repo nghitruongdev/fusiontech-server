48428d7226205d147eeeca37870588a6814e7809
thaian100502 <thaian1000502@gmail.com>
2023-05-30 13:57:38 +0700

    Category and brand using exception

---
+++ product/src/main/java/com/vnco/fusiontech/product/exception/ConflictException.java
@@ -0,0 +1,7 @@
+package com.vnco.fusiontech.product.exception;
+
+public class ConflictException extends RuntimeException {
+    public ConflictException(String message) {
+        super(message);
+    }
+}

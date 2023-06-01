4ac1046a0391bd7c79a14175af2fa62df2d2b660
Le Thanh Tu <letu060802@gmail.com>
2023-06-01 15:25:18 +0700

    them chuc nang review san pham

--- product/src/main/java/com/vnco/fusiontech/product/service/impl/ReviewServiceImpl.java
+++ product/src/main/java/com/vnco/fusiontech/product/service/impl/ReviewServiceImpl.java
@@ -1,19 +1,26 @@
 package com.vnco.fusiontech.product.service.impl;
 
 import com.vnco.fusiontech.product.entity.Review;
+import com.vnco.fusiontech.product.entity.User;
 import com.vnco.fusiontech.product.repository.ReviewRepository;
 import com.vnco.fusiontech.product.service.ReviewService;
+import com.vnco.fusiontech.product.service.UserService;
+import com.vnco.fusiontech.product.web.rest.request.AddProductReviewRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
 
+@Slf4j
 @Slf4j
 @Service
 public class ReviewServiceImpl implements ReviewService {
     @Autowired
     ReviewRepository reviewRepository;
+
+    
 
     
     @Override
@@ -39,7 +46,10 @@ public class ReviewServiceImpl implements ReviewService {
     @Override
     public List<Review> findAllByProductId(Integer productId) {
         return reviewRepository.findReviewsByProductIdIs(productId);
+    public List<Review> findAllByProductId(Integer productId) {
+        return reviewRepository.findReviewsByProductIdIs(productId);
     }
+
 
 
 
--- product/src/main/java/com/vnco/fusiontech/product/web/rest/controller/ProductRestController.java
+++ product/src/main/java/com/vnco/fusiontech/product/web/rest/controller/ProductRestController.java
@@ -51,4 +51,8 @@ public class ProductRestController {
         return products;
     }
 
+
+
+
+
 }

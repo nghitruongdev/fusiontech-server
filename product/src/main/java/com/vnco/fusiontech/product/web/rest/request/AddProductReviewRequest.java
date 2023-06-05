33052c4a30340ff7999d434e344028cc8b826487
xuaanhaor <eji0nkiz@gmail.com>
2023-06-05 17:34:59 +0700

    mod

--- product/src/main/java/com/vnco/fusiontech/product/web/rest/request/AddProductReviewRequest.java
+++ product/src/main/java/com/vnco/fusiontech/product/web/rest/request/AddProductReviewRequest.java
@@ -1,13 +1,6 @@
 package com.vnco.fusiontech.product.web.rest.request;
 
-<<<<<<< HEAD
-=======
-import com.vnco.fusiontech.product.entity.Product;
-import com.vnco.fusiontech.product.entity.User;
 
-import java.time.Instant;
-
->>>>>>> 49bcb8d (them chuc nang review san pham)
 public record AddProductReviewRequest(int id, Integer userId, Integer productId, int rating, String comment) {
 
 }

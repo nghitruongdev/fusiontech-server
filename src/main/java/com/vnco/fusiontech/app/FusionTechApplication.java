e9347589bc129395d46f7af8e7b4f3917d618bea
xuaanhaor <eji0nkiz@gmail.com>
2023-06-05 14:37:47 +0700

    fusiontech application modified

--- src/main/java/com/vnco/fusiontech/app/FusionTechApplication.java
+++ src/main/java/com/vnco/fusiontech/app/FusionTechApplication.java
@@ -3,16 +3,17 @@ package com.vnco.fusiontech.app;
 import com.vnco.fusiontech.cart.CartModuleConfiguration;
 import com.vnco.fusiontech.product.ProductModuleConfiguration;
 import com.vnco.fusiontech.user.UserModuleConfiguration;
+import com.vnco.fusiontech.auth.AuthModuleConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-
 @SpringBootApplication
 @Import (
         {
                 CartModuleConfiguration.class,
                 ProductModuleConfiguration.class,
-                UserModuleConfiguration.class
+                UserModuleConfiguration.class,
+                AuthModuleConfiguration.class
         }
 )
 public class FusionTechApplication {

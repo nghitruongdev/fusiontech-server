ad486baa45386a9d4981248ef9df7fbfc15ff700
xuaanhaor <eji0nkiz@gmail.com>
2023-06-14 13:58:19 +0700

    authority/role added

--- user/src/main/java/com/vnco/fusiontech/user/repository/UserRepository.java
+++ user/src/main/java/com/vnco/fusiontech/user/repository/UserRepository.java
@@ -4,8 +4,10 @@ import com.vnco.fusiontech.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
 import java.util.UUID;
 
-@RepositoryRestResource
+//@RepositoryRestResource
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findById(UUID id);
 }
